a794327309dacbaf9f7cb573a34a89666a006cc4
benbraide <benbraide@yahoo.com>
2017-06-18 08:59:27 -0600

    Created threading::message_loop

---
+++ windows/windows/threading/thread_message_loop.cpp
@@ -0,0 +1,69 @@
+#include "thread_message_loop.h"
+
+winpp::threading::message_loop::message_loop()
+	: id_(id_type::current()), status_(status_info{ false }){}
+
+winpp::threading::message_loop::~message_loop() = default;
+
+int winpp::threading::message_loop::run(){
+	auto idle_index = 0;
+	while (!is_stopped_()){
+		task_stack_.execute();//Execute pending tasks
+		if (queue_.peek(cache_, message_queue::peek_type::remove)){//Not idle
+			if (!cache_.is_quit()){//Forward message
+				if (cache_.owner() == nullptr)//Thread message
+					dispatch_thread_message_();
+				else if (!is_filtered_message_())
+					dispatch_();
+
+				idle_index = 0;//Reset idle index
+			}
+			else{//Quit received
+				status_.return_value = cache_.wparam<int>();
+				break;
+			}
+		}
+		else if (!on_idle_(idle_index++))//No more idle tasks -- wait for event
+			queue_.wait();
+	}
+
+	return status_.return_value;
+}
+
+int winpp::threading::message_loop::result() const{
+	return status_.return_value;
+}
+
+const winpp::threading::message_loop::id_type &winpp::threading::message_loop::id() const{
+	return id_;
+}
+
+void winpp::threading::message_loop::add_task(task_type task){
+	task_stack_.add(task);
+}
+
+bool winpp::threading::message_loop::is_filtered_message_() const{
+	return false;
+}
+
+void winpp::threading::message_loop::translate_() const{
+	cache_.translate();
+}
+
+void winpp::threading::message_loop::dispatch_(){
+	translate_();
+	cache_.dispatch();
+}
+
+void winpp::threading::message_loop::dispatch_thread_message_(){
+	translate_();
+	cache_.dispatch();
+}
+
+bool winpp::threading::message_loop::on_idle_(int index){
+	return false;
+}
+
+bool winpp::threading::message_loop::is_stopped_() const{
+	return true;
+}
---
+++ windows/windows/threading/thread_message_loop.h
@@ -0,0 +1,106 @@
+#pragma once
+
+#ifndef WINPP_THREAD_MESSAGE_LOOP_H
+#define WINPP_THREAD_MESSAGE_LOOP_H
+
+#include "thread_task_stack.h"
+#include "thread_message_queue.h"
+
+namespace winpp{
+	namespace threading{
+		class message_loop{
+		public:
+			typedef id id_type;
+			typedef wrappers::msg msg_type;
+
+			typedef msg_type::uint_type uint_type;
+			typedef msg_type::wparam_type wparam_type;
+			typedef msg_type::lparam_type lparam_type;
+
+			typedef task_stack::callback_type task_type;
+
+			struct status_info{
+				bool is_posted;
+				int return_value;
+			};
+
+			message_loop();
+
+			virtual ~message_loop();
+
+			virtual int run();
+
+			template <typename arg_wparam_type = wparam_type, typename arg_lparam_type = lparam_type>
+			bool post_message(uint_type msg, const arg_wparam_type &wparam, const arg_lparam_type &lparam) const{
+				return (::PostThreadMessageW(id_, msg, (wparam_type)wparam, (lparam_type)lparam) != FALSE);
+			}
+
+			virtual int result() const;
+
+			virtual const id_type &id() const;
+
+			virtual void add_task(task_type task);
+
+			template <typename return_type>
+			return_type execute_task(std::function<return_type()> task){
+				return execute_task_(std::integral_constant<bool, std::is_same<return_type, void>::value>(), task);
+			}
+
+		protected:
+			virtual bool is_filtered_message_() const;
+
+			virtual void translate_() const;
+
+			virtual void dispatch_();
+
+			virtual void dispatch_thread_message_();
+
+			virtual bool on_idle_(int index);
+
+			virtual bool is_stopped_() const;
+
+			template <typename return_type>
+			return_type execute_task_(std::false_type, std::function<return_type()> task){
+				return_type value;
+				if (id_type::current() != id_){
+					event_object<false> ready(L"");
+					task_stack_.add([&]{
+						value = task();
+						ready.set();
+					});
+
+					queue_.wake();//Release message loop
+					ready.wait();
+				}
+				else//Same thread
+					value = task();
+
+				return value;
+			}
+
+			template <typename return_type>
+			return_type execute_task_(std::true_type, std::function<return_type()> task){
+				if (id_type::current() != id_){
+					event_object<false> ready(L"");
+					task_stack_.add([&]{
+						task();
+						ready.set();
+					});
+
+					queue_.wake();//Release message loop
+					ready.wait();
+				}
+				else//Same thread
+					task();
+			}
+
+			id_type id_;
+			message_queue queue_;
+			task_stack task_stack_;
+			msg_type cache_;
+			status_info status_;
+		};
+	}
+}
+
+#endif /* !WINPP_THREAD_MESSAGE_LOOP_H */
